7c8f0df03c784b322c1cbdd66221176e11a1cbd8
khee2 <21keunghee@gmail.com>
2024-08-25 18:01:15 +0900

    remove: 구글 맵 관련 불필요한 파일 삭제

--- ios/AppDelegate.m
+++
@@ -1,7 +0,0 @@
-#import <GoogleMaps/GoogleMaps.h>
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-  [GMSServices provideAPIKey:@"YOUR_GOOGLE_MAPS_API_KEY"];
-  return YES;
-}
